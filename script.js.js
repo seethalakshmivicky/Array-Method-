9d89f8548f1072103c2e93beb0b09ba5c68cdc2e
seethalakshmivicky <seethanathanmca@gmail.com>
2023-10-06 10:21:45 +0530

    file updated

---
+++ script.js.js
@@ -0,0 +1,14 @@
+ let MathScore = [
+    ['John Doe', 20, 60, 'A'],
+    ['Jane Doe', 10, 52, 'B'],
+    ['Petr Chess', 5, 24, 'F'],
+    ['Ling Jess', 28, 43, 'A'],
+    ['Ben Liard', 16, 51, 'B']
+];
+let output=MathScore.map((v,i)=>
+{
+ return v[0];
+});
+console.log(output);
+
+
